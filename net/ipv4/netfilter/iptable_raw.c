63d4d6836c4bf204a70a68f07c76abc016aa72de
GaoFeng <gfree.wind@gmail.com>
2018-05-25 22:47:04 +0800

    net: netfilter: 完成ipv4的raw表的注释和说明
    完成ipv4的raw表的注释和说明，ipv6的raw表除数据结构外，与ipv4区别不大。

--- net/ipv4/netfilter/iptable_raw.c
+++ net/ipv4/netfilter/iptable_raw.c
@@ -17,6 +17,11 @@ static bool raw_before_defrag __read_mostly;
 MODULE_PARM_DESC(raw_before_defrag, "Enable raw table before defrag");
 module_param(raw_before_defrag, bool, 0000);
 
+/*
+raw表是netfilter不太常用的表，其优先级非常高。常用于target NOTRACK
+这里有2个raw表的定义，一个是在defrag（处理分片）之前，一个是在之后。
+由加载这个模块时的参数raw_before_defrag而定，默认是在defrag之后。
+*/
 static const struct xt_table packet_raw = {
 	.name = "raw",
 	.valid_hooks =  RAW_VALID_HOOKS,
@@ -40,6 +45,7 @@ static unsigned int
 iptable_raw_hook(void *priv, struct sk_buff *skb,
 		 const struct nf_hook_state *state)
 {
+	// 这个hook函数很简单，就是执行table中的规则
 	return ipt_do_table(skb, state, state->net->ipv4.iptable_raw);
 }
 
@@ -57,12 +63,16 @@ static int __net_init iptable_raw_table_init(struct net *net)
 	if (net->ipv4.iptable_raw)
 		return 0;
 
+	/*
+	repl的类型struct ipt_replace是iptables用户态针对一个表的规则的集合。
+	这里初始化这个规则结构
+	*/
 	repl = ipt_alloc_initial_table(table);
 	if (repl == NULL)
 		return -ENOMEM;
 	ret = ipt_register_table(net, table, repl, rawtable_ops,
-				 &net->ipv4.iptable_raw);
-	kfree(repl);
+				 &net->ipv4.iptable_raw); // 注册表，同时会把repl转换成dataplane时的结构
+	kfree(repl); // repl已经无用了（前面已经转换成了dataplane的结构），可以安全释放。
 	return ret;
 }
 
@@ -89,6 +99,10 @@ static int __init iptable_raw_init(void)
 		pr_info("Enabling raw table before defrag\n");
 	}
 
+	/*
+	为每个table需要需要的hook创建了nf_hook_ops。对于raw表来说，就是prerouting和localout。
+	这里的rawtable_ops会在注册raw表时使用。
+	*/
 	rawtable_ops = xt_hook_ops_alloc(table, iptable_raw_hook);
 	if (IS_ERR(rawtable_ops))
 		return PTR_ERR(rawtable_ops);
