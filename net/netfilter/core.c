98cf2bf2827aae12a39d9b697c9aeeb982b21550
GaoFeng <gfree.wind@gmail.com>
2018-07-02 21:17:41 +0800

    netfilter: 完全注释net/netfilter/core.c
    net/netfilter/core.c是netfilter的核心源文件，其主要负责hook的register
    和unregister，以及匹配。

--- net/netfilter/core.c
+++ net/netfilter/core.c
@@ -30,15 +30,15 @@
 const struct nf_ipv6_ops __rcu *nf_ipv6_ops __read_mostly;
 EXPORT_SYMBOL_GPL(nf_ipv6_ops);
 
-DEFINE_PER_CPU(bool, nf_skb_duplicated);
+DEFINE_PER_CPU(bool, nf_skb_duplicated);//表示该skb是否已经复制了，用于dup模块，避免再次复制。
 EXPORT_SYMBOL_GPL(nf_skb_duplicated);
 
 #ifdef HAVE_JUMP_LABEL
-struct static_key nf_hooks_needed[NFPROTO_NUMPROTO][NF_MAX_HOOKS];
+struct static_key nf_hooks_needed[NFPROTO_NUMPROTO][NF_MAX_HOOKS];//作为bool值使用。用于加速空的hook匹配。
 EXPORT_SYMBOL(nf_hooks_needed);
 #endif
 
-static DEFINE_MUTEX(nf_hook_mutex);
+static DEFINE_MUTEX(nf_hook_mutex);//用于保护hook，如register，unregister
 
 /* max hooks per family/hooknum */
 #define MAX_HOOK_COUNT		1024
@@ -46,9 +46,29 @@ static DEFINE_MUTEX(nf_hook_mutex);
 #define nf_entry_dereference(e) \
 	rcu_dereference_protected(e, lockdep_is_held(&nf_hook_mutex))
 
-static struct nf_hook_entries *allocate_hook_entries_size(u16 num)
+static struct nf_hook_entries *allocate_hook_entries_size(u16 num)//申请hook的内存空间
 {
 	struct nf_hook_entries *e;
+	/*
+	注意这里的内存布局
+	+--------------------------+
+	+ nf_hook_entries结构      +
+	+--------------------------+
+	+ nf_hook_entry            +
+	+--------------------------+
+	+ ...... ......            +
+	+--------------------------+
+	+ nf_hook_entry（第N个）   +
+	+-----------------  -------+
+	+ nf_hook_ops              +
+	+--------------------------+
+	+ ...... ......            +
+	+--------------------------+
+	+ nf_hook_ops（第N个）     +
+	+--------------------------+
+	+ nf_hook_entries_rcu_head +
+	+--------------------------+
+	*/
 	size_t alloc = sizeof(*e) +
 		       sizeof(struct nf_hook_entry) * num +
 		       sizeof(struct nf_hook_ops *) * num +
@@ -63,7 +83,7 @@ static struct nf_hook_entries *allocate_hook_entries_size(u16 num)
 	return e;
 }
 
-static void __nf_hook_entries_free(struct rcu_head *h)
+static void __nf_hook_entries_free(struct rcu_head *h)//释放hook的内存空间
 {
 	struct nf_hook_entries_rcu_head *head;
 
@@ -71,7 +91,7 @@ static void __nf_hook_entries_free(struct rcu_head *h)
 	kvfree(head->allocation);
 }
 
-static void nf_hook_entries_free(struct nf_hook_entries *e)
+static void nf_hook_entries_free(struct nf_hook_entries *e)//释放hook的封装函数，转换为head，调用rcu释放。
 {
 	struct nf_hook_entries_rcu_head *head;
 	struct nf_hook_ops **ops;
@@ -89,19 +109,19 @@ static void nf_hook_entries_free(struct nf_hook_entries *e)
 
 static unsigned int accept_all(void *priv,
 			       struct sk_buff *skb,
-			       const struct nf_hook_state *state)
+			       const struct nf_hook_state *state)//用于dummy hook
 {
 	return NF_ACCEPT; /* ACCEPT makes nf_hook_slow call next hook */
 }
 
-static const struct nf_hook_ops dummy_ops = {
+static const struct nf_hook_ops dummy_ops = {//删除hook时，使用dummy_ops占位，简化代码逻辑
 	.hook = accept_all,
 	.priority = INT_MIN,
 };
 
 static struct nf_hook_entries *
 nf_hook_entries_grow(const struct nf_hook_entries *old,
-		     const struct nf_hook_ops *reg)
+		     const struct nf_hook_ops *reg)//增加新的hook条目reg
 {
 	unsigned int i, alloc_entries, nhooks, old_entries;
 	struct nf_hook_ops **orig_ops = NULL;
@@ -109,40 +129,41 @@ nf_hook_entries_grow(const struct nf_hook_entries *old,
 	struct nf_hook_entries *new;
 	bool inserted = false;
 
-	alloc_entries = 1;
+	alloc_entries = 1;//至少要申请一个条目空间
 	old_entries = old ? old->num_hook_entries : 0;
 
 	if (old) {
 		orig_ops = nf_hook_entries_get_hook_ops(old);
 
-		for (i = 0; i < old_entries; i++) {
+		for (i = 0; i < old_entries; i++) {//计算旧的hook条目个数
 			if (orig_ops[i] != &dummy_ops)
 				alloc_entries++;
 		}
 	}
 
-	if (alloc_entries > MAX_HOOK_COUNT)
+	if (alloc_entries > MAX_HOOK_COUNT) //条目个数限制检查
 		return ERR_PTR(-E2BIG);
 
-	new = allocate_hook_entries_size(alloc_entries);
+	new = allocate_hook_entries_size(alloc_entries);//申请新的hook空间
 	if (!new)
 		return ERR_PTR(-ENOMEM);
 
-	new_ops = nf_hook_entries_get_hook_ops(new);
+	new_ops = nf_hook_entries_get_hook_ops(new);//得到hook_ops的起始位置
 
 	i = 0;
 	nhooks = 0;
+	/* 复制旧有的hook条目 */
 	while (i < old_entries) {
-		if (orig_ops[i] == &dummy_ops) {
+		if (orig_ops[i] == &dummy_ops) {//跳过占位的dummy_ops
 			++i;
 			continue;
 		}
 
-		if (inserted || reg->priority > orig_ops[i]->priority) {
+		if (inserted || reg->priority > orig_ops[i]->priority) {//reg的优先级低于旧有的，复制旧有的
 			new_ops[nhooks] = (void *)orig_ops[i];
 			new->hooks[nhooks] = old->hooks[i];
 			i++;
-		} else {
+		} else {//reg的优先级高于旧有的，插入reg，并设置标志位
 			new_ops[nhooks] = (void *)reg;
 			new->hooks[nhooks].hook = reg->hook;
 			new->hooks[nhooks].priv = reg->priv;
@@ -151,7 +172,7 @@ nf_hook_entries_grow(const struct nf_hook_entries *old,
 		nhooks++;
 	}
 
-	if (!inserted) {
+	if (!inserted) {//前面没有插入reg，则在末尾追加。
 		new_ops[nhooks] = (void *)reg;
 		new->hooks[nhooks].hook = reg->hook;
 		new->hooks[nhooks].priv = reg->priv;
@@ -160,7 +181,7 @@ nf_hook_entries_grow(const struct nf_hook_entries *old,
 	return new;
 }
 
-static void hooks_validate(const struct nf_hook_entries *hooks)
+static void hooks_validate(const struct nf_hook_entries *hooks)//检查hooks的合法性（目前仅是比较优先级）
 {
 #ifdef CONFIG_DEBUG_KERNEL
 	struct nf_hook_ops **orig_ops;
@@ -181,23 +202,24 @@ static void hooks_validate(const struct nf_hook_entries *hooks)
 #endif
 }
 
+/* 插入新的hook */
 int nf_hook_entries_insert_raw(struct nf_hook_entries __rcu **pp,
 				const struct nf_hook_ops *reg)
 {
 	struct nf_hook_entries *new_hooks;
 	struct nf_hook_entries *p;
 
-	p = rcu_dereference_raw(*pp);
-	new_hooks = nf_hook_entries_grow(p, reg);
+	p = rcu_dereference_raw(*pp);//得到当前hook
+	new_hooks = nf_hook_entries_grow(p, reg);//生成新的hook
 	if (IS_ERR(new_hooks))
 		return PTR_ERR(new_hooks);
 
 	hooks_validate(new_hooks);
 
-	rcu_assign_pointer(*pp, new_hooks);
+	rcu_assign_pointer(*pp, new_hooks);//替换当前hook
 
 	BUG_ON(p == new_hooks);
-	nf_hook_entries_free(p);
+	nf_hook_entries_free(p);//是否旧有hook（RCU释放）
 	return 0;
 }
 EXPORT_SYMBOL_GPL(nf_hook_entries_insert_raw);
@@ -219,7 +241,7 @@ EXPORT_SYMBOL_GPL(nf_hook_entries_insert_raw);
  * Returns address to free, or NULL.
  */
 static void *__nf_hook_entries_try_shrink(struct nf_hook_entries *old,
-					  struct nf_hook_entries __rcu **pp)
+					  struct nf_hook_entries __rcu **pp)//尝试缩减hook占用的空间
 {
 	unsigned int i, j, skip = 0, hook_entries;
 	struct nf_hook_entries *new = NULL;
@@ -230,24 +252,25 @@ static void *__nf_hook_entries_try_shrink(struct nf_hook_entries *old,
 		return NULL;
 
 	orig_ops = nf_hook_entries_get_hook_ops(old);
-	for (i = 0; i < old->num_hook_entries; i++) {
+	for (i = 0; i < old->num_hook_entries; i++) {//统计有多少个dummy_ops
 		if (orig_ops[i] == &dummy_ops)
 			skip++;
 	}
 
 	/* if skip == hook_entries all hooks have been removed */
 	hook_entries = old->num_hook_entries;
-	if (skip == hook_entries)
+	if (skip == hook_entries)//表示所有的hook都被删除了
 		goto out_assign;
 
-	if (skip == 0)
+	if (skip == 0)//没有dummy_ops
 		return NULL;
 
-	hook_entries -= skip;
-	new = allocate_hook_entries_size(hook_entries);
+	hook_entries -= skip;//得到真实有效的hook个数
+	new = allocate_hook_entries_size(hook_entries);//申请hook空间
 	if (!new)
 		return NULL;
 
+	/* 将当前hook条目复制到新的hook空间中 */
 	new_ops = nf_hook_entries_get_hook_ops(new);
 	for (i = 0, j = 0; i < old->num_hook_entries; i++) {
 		if (orig_ops[i] == &dummy_ops)
@@ -258,13 +281,13 @@ static void *__nf_hook_entries_try_shrink(struct nf_hook_entries *old,
 	}
 	hooks_validate(new);
 out_assign:
-	rcu_assign_pointer(*pp, new);
+	rcu_assign_pointer(*pp, new);//使用新的hook替换当前的
 	return old;
 }
 
 static struct nf_hook_entries __rcu **
 nf_hook_entry_head(struct net *net, int pf, unsigned int hooknum,
-		   struct net_device *dev)
+		   struct net_device *dev)//得到指定协议族的hook head的地址（注意这里是个二级指针）
 {
 	switch (pf) {
 	case NFPROTO_NETDEV:
@@ -311,7 +334,7 @@ nf_hook_entry_head(struct net *net, int pf, unsigned int hooknum,
 }
 
 static int __nf_register_net_hook(struct net *net, int pf,
-				  const struct nf_hook_ops *reg)
+				  const struct nf_hook_ops *reg)//注册新的hook
 {
 	struct nf_hook_entries *p, *new_hooks;
 	struct nf_hook_entries __rcu **pp;
@@ -326,17 +349,17 @@ static int __nf_register_net_hook(struct net *net, int pf,
 			return -EINVAL;
 	}
 
-	pp = nf_hook_entry_head(net, pf, reg->hooknum, reg->dev);
+	pp = nf_hook_entry_head(net, pf, reg->hooknum, reg->dev);//得到指定协议族的hook head地址
 	if (!pp)
 		return -EINVAL;
 
 	mutex_lock(&nf_hook_mutex);
 
 	p = nf_entry_dereference(*pp);
-	new_hooks = nf_hook_entries_grow(p, reg);
+	new_hooks = nf_hook_entries_grow(p, reg);//增加新的hook
 
 	if (!IS_ERR(new_hooks))
-		rcu_assign_pointer(*pp, new_hooks);
+		rcu_assign_pointer(*pp, new_hooks);//没有出错的话，就替换当前hook
 
 	mutex_unlock(&nf_hook_mutex);
 	if (IS_ERR(new_hooks))
@@ -348,10 +371,10 @@ static int __nf_register_net_hook(struct net *net, int pf,
 		net_inc_ingress_queue();
 #endif
 #ifdef HAVE_JUMP_LABEL
-	static_key_slow_inc(&nf_hooks_needed[pf][reg->hooknum]);
+	static_key_slow_inc(&nf_hooks_needed[pf][reg->hooknum]);//增加计数，表示该hook点有hook回调
 #endif
 	BUG_ON(p == new_hooks);
-	nf_hook_entries_free(p);
+	nf_hook_entries_free(p);//是否旧有hook
 	return 0;
 }
 
@@ -365,7 +388,7 @@ static int __nf_register_net_hook(struct net *net, int pf,
  * Therefore replace the to-be-removed hook with a dummy hook.
  */
 static bool nf_remove_net_hook(struct nf_hook_entries *old,
-			       const struct nf_hook_ops *unreg)
+			       const struct nf_hook_ops *unreg)//删除hook
 {
 	struct nf_hook_ops **orig_ops;
 	unsigned int i;
@@ -374,7 +397,7 @@ static bool nf_remove_net_hook(struct nf_hook_entries *old,
 	for (i = 0; i < old->num_hook_entries; i++) {
 		if (orig_ops[i] != unreg)
 			continue;
-		WRITE_ONCE(old->hooks[i].hook, accept_all);
+		WRITE_ONCE(old->hooks[i].hook, accept_all);//用dummy_ops占位
 		WRITE_ONCE(orig_ops[i], &dummy_ops);
 		return true;
 	}
@@ -383,7 +406,7 @@ static bool nf_remove_net_hook(struct nf_hook_entries *old,
 }
 
 static void __nf_unregister_net_hook(struct net *net, int pf,
-				     const struct nf_hook_ops *reg)
+				     const struct nf_hook_ops *reg)//有锁保护的删除hook函数
 {
 	struct nf_hook_entries __rcu **pp;
 	struct nf_hook_entries *p;
@@ -400,7 +423,7 @@ static void __nf_unregister_net_hook(struct net *net, int pf,
 		return;
 	}
 
-	if (nf_remove_net_hook(p, reg)) {
+	if (nf_remove_net_hook(p, reg)) {//删除hook
 #ifdef CONFIG_NETFILTER_INGRESS
 		if (pf == NFPROTO_NETDEV && reg->hooknum == NF_NETDEV_INGRESS)
 			net_dec_ingress_queue();
@@ -412,16 +435,16 @@ static void __nf_unregister_net_hook(struct net *net, int pf,
 		WARN_ONCE(1, "hook not found, pf %d num %d", pf, reg->hooknum);
 	}
 
-	p = __nf_hook_entries_try_shrink(p, pp);
+	p = __nf_hook_entries_try_shrink(p, pp);//尝试减少hook占用的空间
 	mutex_unlock(&nf_hook_mutex);
 	if (!p)
 		return;
 
-	nf_queue_nf_hook_drop(net);
+	nf_queue_nf_hook_drop(net);//是否对应
 	nf_hook_entries_free(p);
 }
 
-void nf_unregister_net_hook(struct net *net, const struct nf_hook_ops *reg)
+void nf_unregister_net_hook(struct net *net, const struct nf_hook_ops *reg)//unregister hook的接口函数
 {
 	if (reg->pf == NFPROTO_INET) {
 		__nf_unregister_net_hook(net, NFPROTO_IPV4, reg);
@@ -433,19 +456,19 @@ void nf_unregister_net_hook(struct net *net, const struct nf_hook_ops *reg)
 EXPORT_SYMBOL(nf_unregister_net_hook);
 
 void nf_hook_entries_delete_raw(struct nf_hook_entries __rcu **pp,
-				const struct nf_hook_ops *reg)
+				const struct nf_hook_ops *reg)//删除一个hook
 {
 	struct nf_hook_entries *p;
 
 	p = rcu_dereference_raw(*pp);
 	if (nf_remove_net_hook(p, reg)) {
-		p = __nf_hook_entries_try_shrink(p, pp);
+		p = __nf_hook_entries_try_shrink(p, pp);//删除成功，则尝试缩减hook空间
 		nf_hook_entries_free(p);
 	}
 }
 EXPORT_SYMBOL_GPL(nf_hook_entries_delete_raw);
 
-int nf_register_net_hook(struct net *net, const struct nf_hook_ops *reg)
+int nf_register_net_hook(struct net *net, const struct nf_hook_ops *reg)//注册hook
 {
 	int err;
 
@@ -470,7 +493,7 @@ int nf_register_net_hook(struct net *net, const struct nf_hook_ops *reg)
 EXPORT_SYMBOL(nf_register_net_hook);
 
 int nf_register_net_hooks(struct net *net, const struct nf_hook_ops *reg,
-			  unsigned int n)
+			  unsigned int n)//批量注册hook
 {
 	unsigned int i;
 	int err = 0;
@@ -490,7 +513,7 @@ err:
 EXPORT_SYMBOL(nf_register_net_hooks);
 
 void nf_unregister_net_hooks(struct net *net, const struct nf_hook_ops *reg,
-			     unsigned int hookcount)
+			     unsigned int hookcount)//批量unregister hook
 {
 	unsigned int i;
 
@@ -536,7 +559,7 @@ int nf_hook_slow(struct sk_buff *skb, struct nf_hook_state *state,
 EXPORT_SYMBOL(nf_hook_slow);
 
 
-int skb_make_writable(struct sk_buff *skb, unsigned int writable_len)
+int skb_make_writable(struct sk_buff *skb, unsigned int writable_len)//保证skb有writable_len的可写长度
 {
 	if (writable_len > skb->len)
 		return 0;
@@ -577,6 +600,7 @@ EXPORT_SYMBOL(ip_ct_attach);
 struct nf_nat_hook __rcu *nf_nat_hook __read_mostly;
 EXPORT_SYMBOL_GPL(nf_nat_hook);
 
+/* 将new skb attach到skb的连接上 */
 void nf_ct_attach(struct sk_buff *new, const struct sk_buff *skb)
 {
 	void (*attach)(struct sk_buff *, const struct sk_buff *);
@@ -591,7 +615,7 @@ void nf_ct_attach(struct sk_buff *new, const struct sk_buff *skb)
 }
 EXPORT_SYMBOL(nf_ct_attach);
 
-void nf_conntrack_destroy(struct nf_conntrack *nfct)
+void nf_conntrack_destroy(struct nf_conntrack *nfct) // 销毁conntrack
 {
 	struct nf_ct_hook *ct_hook;
 
@@ -604,7 +628,7 @@ void nf_conntrack_destroy(struct nf_conntrack *nfct)
 EXPORT_SYMBOL(nf_conntrack_destroy);
 
 /* Built-in default zone used e.g. by modules. */
-const struct nf_conntrack_zone nf_ct_zone_dflt = {
+const struct nf_conntrack_zone nf_ct_zone_dflt = {//默认的conntrack zone
 	.id	= NF_CT_DEFAULT_ZONE_ID,
 	.dir	= NF_CT_DEFAULT_ZONE_DIR,
 };
@@ -622,6 +646,7 @@ __netfilter_net_init(struct nf_hook_entries __rcu **e, int max)
 
 static int __net_init netfilter_net_init(struct net *net)
 {
+	/* 初始化当前namespace的各个协议族的hook */
 	__netfilter_net_init(net->nf.hooks_ipv4, ARRAY_SIZE(net->nf.hooks_ipv4));
 	__netfilter_net_init(net->nf.hooks_ipv6, ARRAY_SIZE(net->nf.hooks_ipv6));
 #ifdef CONFIG_NETFILTER_FAMILY_ARP
@@ -658,15 +683,17 @@ static struct pernet_operations netfilter_net_ops = {
 	.exit = netfilter_net_exit,
 };
 
+/* netfilter 核心的初始化函数。大家可以注意到，只有init，没有fini。现在不少内核模块都是这样了 */
 int __init netfilter_init(void)
 {
 	int ret;
 
+	/* 注册namespace的init和exit回调。创建和销毁namespace时，会自动调用 */
 	ret = register_pernet_subsys(&netfilter_net_ops);
 	if (ret < 0)
 		goto err;
 
-	ret = netfilter_log_init();
+	ret = netfilter_log_init();//netfilter log模块的初始化
 	if (ret < 0)
 		goto err_pernet;
 
