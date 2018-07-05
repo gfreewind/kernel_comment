8f6003bfb3fc97be196fd25f507bf5bd30092e69
GaoFeng <gfree.wind@gmail.com>
2018-07-05 09:21:04 +0800

    netfilter: 注释net/netfilter/nf_conntrack_extend.c
    注释net/netfilter/nf_conntrack_extend.c。这是conntrack的扩展实现。

--- net/netfilter/nf_conntrack_extend.c
+++ net/netfilter/nf_conntrack_extend.c
@@ -17,8 +17,8 @@
 #include <linux/skbuff.h>
 #include <net/netfilter/nf_conntrack_extend.h>
 
-static struct nf_ct_ext_type __rcu *nf_ct_ext_types[NF_CT_EXT_NUM];
-static DEFINE_MUTEX(nf_ct_ext_type_mutex);
+static struct nf_ct_ext_type __rcu *nf_ct_ext_types[NF_CT_EXT_NUM];//conntrack的扩展类型
+static DEFINE_MUTEX(nf_ct_ext_type_mutex);//用于保护上面的数组
 #define NF_CT_EXT_PREALLOC	128u /* conntrack events are on by default */
 
 void nf_ct_ext_destroy(struct nf_conn *ct)
@@ -41,28 +41,28 @@ void nf_ct_ext_destroy(struct nf_conn *ct)
 }
 EXPORT_SYMBOL(nf_ct_ext_destroy);
 
-void *nf_ct_ext_add(struct nf_conn *ct, enum nf_ct_ext_id id, gfp_t gfp)
+void *nf_ct_ext_add(struct nf_conn *ct, enum nf_ct_ext_id id, gfp_t gfp)//给当前conntack增加扩展
 {
 	unsigned int newlen, newoff, oldlen, alloc;
 	struct nf_ct_ext *old, *new;
 	struct nf_ct_ext_type *t;
 
 	/* Conntrack must not be confirmed to avoid races on reallocation. */
-	WARN_ON(nf_ct_is_confirmed(ct));
+	WARN_ON(nf_ct_is_confirmed(ct));//必须是unconfirmed，这样才能保证只有一个cpu在访问该conntrack
 
 	old = ct->ext;
 
-	if (old) {
-		if (__nf_ct_ext_exist(old, id))
+	if (old) {//conntrack已有扩展
+		if (__nf_ct_ext_exist(old, id))//检查是否要添加的扩展也已经存在
 			return NULL;
 		oldlen = old->len;
 	} else {
-		oldlen = sizeof(*new);
+		oldlen = sizeof(*new);//如果没有扩展，则需要计算上nf_ct_ext的大小
 	}
 
 	rcu_read_lock();
 	t = rcu_dereference(nf_ct_ext_types[id]);
-	if (!t) {
+	if (!t) {//检查扩展类型是否注册
 		rcu_read_unlock();
 		return NULL;
 	}
@@ -71,20 +71,21 @@ void *nf_ct_ext_add(struct nf_conn *ct, enum nf_ct_ext_id id, gfp_t gfp)
 	newlen = newoff + t->len;
 	rcu_read_unlock();
 
-	alloc = max(newlen, NF_CT_EXT_PREALLOC);
+	alloc = max(newlen, NF_CT_EXT_PREALLOC);//添加ext时，至少会保证ext有NF_CT_EXT_PREALLOC（128）字节的空间
 	kmemleak_not_leak(old);
-	new = __krealloc(old, alloc, gfp);
+	new = __krealloc(old, alloc, gfp);//这个__krealloc不会释放old指向的内存
 	if (!new)
 		return NULL;
 
-	if (!old) {
+	if (!old) {//初始化nf_ct_ext
 		memset(new->offset, 0, sizeof(new->offset));
 		ct->ext = new;
 	} else if (new != old) {
 		kfree_rcu(old, rcu);
-		rcu_assign_pointer(ct->ext, new);
+		rcu_assign_pointer(ct->ext, new);//替换ext
 	}
 
+	/* 设置新的扩展 */
 	new->offset[id] = newoff;
 	new->len = newlen;
 	memset((void *)new + newoff, 0, newlen - newoff);
@@ -93,7 +94,7 @@ void *nf_ct_ext_add(struct nf_conn *ct, enum nf_ct_ext_id id, gfp_t gfp)
 EXPORT_SYMBOL(nf_ct_ext_add);
 
 /* This MUST be called in process context. */
-int nf_ct_extend_register(const struct nf_ct_ext_type *type)
+int nf_ct_extend_register(const struct nf_ct_ext_type *type)//注册conntrack扩展
 {
 	int ret = 0;
 
@@ -111,11 +112,11 @@ out:
 EXPORT_SYMBOL_GPL(nf_ct_extend_register);
 
 /* This MUST be called in process context. */
-void nf_ct_extend_unregister(const struct nf_ct_ext_type *type)
+void nf_ct_extend_unregister(const struct nf_ct_ext_type *type)//unregister 扩展
 {
 	mutex_lock(&nf_ct_ext_type_mutex);
 	RCU_INIT_POINTER(nf_ct_ext_types[type->id], NULL);
 	mutex_unlock(&nf_ct_ext_type_mutex);
-	synchronize_rcu();
+	synchronize_rcu();//同步rcu，确保当前持有扩展的cpu不会再访问
 }
 EXPORT_SYMBOL_GPL(nf_ct_extend_unregister);
